0e5e298d84b821360f72a4521f8c7514ff933535
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-01-13 10:51:15 -0300

    refactor: update celestial bodies

--- src/celetial-bodies/Earth.tsx
+++ src/celetial-bodies/Earth.tsx
@@ -1,14 +1,15 @@
-import { useLoader } from "@react-three/fiber";
+import { MeshProps, useLoader } from "@react-three/fiber";
 import { Texture, TextureLoader } from "three";
 
-export const Earth = (): JSX.Element => {
+interface EarthProps extends MeshProps {}
+
+export const Earth = (props: EarthProps): JSX.Element => {
   const colorMap: Texture = useLoader(TextureLoader, "earth.jpg");
 
   return (
     <>
       <directionalLight position={[0.5, 0, 0]} />
-      <mesh>
-        {/* Width and height segments for displacementMap */}
+      <mesh {...props}>
         <sphereBufferGeometry args={[1, 100, 100]} />
         <meshStandardMaterial displacementScale={0.2} map={colorMap} />
       </mesh>
--- src/celetial-bodies/Sun.tsx
+++ src/celetial-bodies/Sun.tsx
@@ -1,18 +1,20 @@
 import { Sparkles } from "@react-three/drei";
-import { useFrame, useLoader } from "@react-three/fiber";
+import { MeshProps, useFrame, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
-import { Texture, TextureLoader } from "three";
+import { Mesh, Texture, TextureLoader } from "three";
 
-export const Sun = (): JSX.Element => {
+interface SunProps extends MeshProps {}
+
+export const Sun = (props: SunProps): JSX.Element => {
   const colorMap: Texture = useLoader(TextureLoader, "sun.jpg");
-  const sunRef = useRef(null);
+  const sunRef = useRef<Mesh>(null!);
 
   useFrame(() => (sunRef.current.rotation.y += 0.001));
 
   return (
     <>
       <ambientLight intensity={1} />
-      <mesh ref={sunRef}>
+      <mesh {...props} ref={sunRef}>
         <Sparkles scale={2} count={100} />
         <sphereBufferGeometry args={[1, 100, 100]} />
         <meshStandardMaterial map={colorMap} />
