ee51545b10a2611813f415f71c12b0ebb7078ec8
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-01-14 20:53:51 -0300

    feat: create planet Jupiter

---
+++ src/universe/solar-system/Jupiter.tsx
@@ -0,0 +1,16 @@
+import { MeshProps, useLoader } from "@react-three/fiber";
+import { Texture, TextureLoader } from "three";
+import jupiterMap from "../../assets/textures/jupiter-map.jpg";
+
+interface JupiterProps extends MeshProps {}
+
+export const Jupiter = (props: JupiterProps): JSX.Element => {
+  const colorMap: Texture = useLoader(TextureLoader, jupiterMap);
+
+  return (
+    <mesh {...props}>
+      <sphereGeometry args={[1, 100, 100]} />
+      <meshStandardMaterial map={colorMap} />
+    </mesh>
+  );
+};
--- src/universe/solar-system/SolarSystem.tsx
+++ src/universe/solar-system/SolarSystem.tsx
@@ -1,4 +1,5 @@
 import { Earth } from "./Earth";
+import { Jupiter } from "./Jupiter";
 import { Mars } from "./Mars";
 import { Mercury } from "./Mercury";
 import { Sun } from "./Sun";
@@ -11,7 +12,8 @@ export const SolarSystem = (): JSX.Element => {
       <Mercury position={[40, 0, 0]} />
       <Venus position={[60, 0, 0]} />
       <Earth position={[-20, 0, 0]} />
-      <Mars position={[0, 0, 0]} />
+      <Mars position={[100, 0, 0]} />
+      <Jupiter position={[0, 0, 0]} />
     </>
   );
 };
