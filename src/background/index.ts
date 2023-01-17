ccc2e800dbd7012488e3543e209bb66a850114bc
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-01-17 12:50:07 -0300

    style: rename background file

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,13 +1,10 @@
 import { CubeTextureLoader } from "three";
-import { scene } from "../scene/scene";
 
 const background = {
   path: "src/assets/textures/background/",
   images: ["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"],
 };
 
-const backgroundTexture = new CubeTextureLoader()
+export const backgroundTexture = new CubeTextureLoader()
   .setPath(background.path)
   .load(background.images);
-
-scene.background = backgroundTexture;
